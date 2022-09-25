3d58a70a448937bf7fa858027779d0771925142e
Kyu Lee <kwooler@gmail.com>
2022-09-25 14:39:02 -0700

    CS1Q61-Q2

---
+++ q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//********************
+// Declare Function Prototypes
+//********************
+
+int main()
+{
+}
+
+// ******************************
+// Implement all your functions here
+// ******************************
