4a7684edbfcc8dac3ef2a037f99ff752752234f4
Kyu Lee <kwooler@gmail.com>
2022-12-13 15:24:14 -0800

    Q6Q2

---
+++ q2.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//********************
+void getInput(int &, int &, int &);
+int findMin(int, int, int);
+void printResult(int n1, int n2, int n3, int min);
+
+void getInput(int &n1, int &n2, int &n3)
+{
+	cout << "Enter three numbers: ";
+	cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3)
+{
+	int min;
+	min = n1;
+	(n2 < min) ? min = n2 : min = min;
+	(n3 < min) ? min = n3 : min = min;
+	return min;
+}
+void printResult(int n1, int n2, int n3, int min)
+{
+	cout << n1 << "\t" << n2 << "\t" << n3 << "\t" << min << "\t" << endl;
+}
+
+// ******************************
+// Implement all your functions here
+// ******************************
